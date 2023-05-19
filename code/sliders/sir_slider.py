7fc5e9533772dfb0c75b197372d145338c4330be
ItyTell <nickkolomietsdooor@gmail.com>
2023-05-19 11:22:43 +0300

    refactoring

--- code/sliders/sir_slider.py
+++ code/sliders/sir_slider.py
@@ -13,23 +13,9 @@ sir = ctypes.CDLL("C:\\Users\\nickk\\course_work\\code\\sliders\\sir\\sir.so")
 sir.sir.argtypes = [c_int, c_float, c_float, c_int, c_float]
 sir.sir.restype = POINTER(c_float)
 
-f = open("data.txt")
-data = f.readline().split()
-for i in range(len(data)):
-    data[i] = int(data[i])
-
 T = 1151
 dt = 0.1
 N = 647601
-
-def f1(s, i, r, beta, gama):
-    return -beta * s * i / N
-
-def f2(s, i, r, beta, gama):
-    return beta * s * i / N - gama * i
-
-def f3(s, i, r, beta, gama):
-    return gama * i
 
 def g(beta, gama):
     size = int(T/dt)
@@ -38,55 +24,13 @@ def g(beta, gama):
     sir.free_memory(result)
     return answer 
 
-
-
-t = np.array([i for i in range(T)])
-
 init_beta = 0.12
 init_gama = 0.11
 
-fig, ax = plt.subplots()
-line, = ax.plot(t, g(init_beta, init_gama), lw=2)
-ax.plot(t, data, color='r')
-ax.set_xlabel('Time [d]')
-plt.ylim(0, N / 2)
+graph = Graph("sir", g, T, N)
 
-fig.subplots_adjust(bottom=0.5)
-
-gama = Param(fig, 'gama', init_gama, 0, 1)
-
-beta = Param(fig, 'beta', init_beta, 0, 1)
+graph.add_parametr("gama", init_gama, 0, 1)
+graph.add_parametr("beta", init_beta, 0, 1)
 
 
-def update(val):
-    line.set_ydata(g(beta.slider.val, gama.slider.val))
-    fig.canvas.draw_idle()
-
-gama.slider.on_changed(update)
-beta.slider.on_changed(update)
-
-resetax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
-button = Button(resetax, 'Reset', hovercolor='0.975')
-
-def reset(event):
-    gama.slider.reset()
-    beta.slider.reset()
-button.on_clicked(reset)
-
-
-
-saveax = fig.add_axes([0.6, 0.025, 0.1, 0.04])
-sve = Button(saveax, 'Save', hovercolor='0.975')
-
-def save_params(event):
-    name = "sir"
-    file = open("C:\\Users\\nickk\\course_work\\code\\sliders\\"+ name +"_params.txt", 'w')
-    for slider in Param.sliders:
-        file.write(slider.name + " " + str(slider.slider.val) + "\n")
-    file.close()
-
-sve.on_clicked(save_params)
-
-
-
-plt.show()
+graph.preset()
--- code/sliders/slider.py
+++ code/sliders/slider.py
@@ -1,22 +1,84 @@
 
 from matplotlib.widgets import Slider, Button
+import matplotlib.pyplot as plt
+import numpy as np
 
 class Param():
 
-    n = 0
-    sliders = []
 
-    def __init__(self, fig, label, init_val, min, max) -> None:
-        self.ax = fig.add_axes([0.1, 0.4 - Param.n * 0.05, 0.8, 0.02])
+    def __init__(self, fig, label, init_val, min, max, n) -> None:
+        self.ax = fig.add_axes([0.1, 0.4 - n * 0.05, 0.8, 0.02])
         self.slider = Slider(
                             ax=self.ax,
                             label=label,
                             valmin=min,
                             valmax=max,
                             valinit=init_val)
-        Param.n += 1
-        Param.sliders.append(self)
-
         self.name = label
+    
+
+class Graph():
+
+    def __init__(self, name, f, T, N) -> None:
+        self.fig, self.ax = plt.subplots()
+        self.n = 0
+        self.params = []
+        self.name = name
+        self.function = f
+        self.T = T
+        self.N = N
+        self.inits = []
+        self.save = Button(self.fig.add_axes([0.6, 0.025, 0.1, 0.04]), 'Save', hovercolor='0.975')
+        self.reset = Button(self.fig.add_axes([0.8, 0.025, 0.1, 0.04]), 'Reset', hovercolor='0.975')
+    
+    def add_parametr(self, label, init_val, min, max):
+        self.params.append(Param(self.fig, label, init_val, min, max, self.n))
+        self.inits.append(init_val)
+        self.n += 1
+    
+    def save_params(self, event):
+        name = "sir"
+        file = open("C:\\Users\\nickk\\course_work\\code\\sliders\\"+ name +"_params.txt", 'w')
+        for slider in Param.sliders:
+            file.write(slider.name + " " + str(slider.slider.val) + "\n")
+        file.close()
+
+    def reset(self, event):
+        for param in self.params:
+            param.slider.reset()
+    
+    def update(self, event):
+        values = []
+        for param in self.params:
+            values.append(param.slider.val)
+        self.line.set_ydata(self.function(*values))
+        self.fig.canvas.draw_idle()
+
+    
+    def preset(self):
+        file = open("data.txt")
+        data = file.readline().split()
+        for i in range(len(data)):
+            data[i] = int(data[i])
+        file.close()
+
+        self.t = np.array([i for i in range(self.T)])
+
+        self.line, = self.ax.plot(self.t, self.function(*self.inits), lw=2)
+
+
+        self.ax.plot(self.t, data, color='r')
+        self.ax.set_xlabel('Time [d]')
+        plt.ylim(0, self.N / 2)
+        self.fig.subplots_adjust(bottom=0.5)
+
+        self.save.on_clicked(self.save_params)
+        self.reset.on_clicked(self.reset)
+        for param in self.params:
+            param.slider.on_changed(self.update)
+        plt.show()
+
+
+
 
 
