70ff5edbe055b905738c7eb84b1e0738875f5a9f
ItyTell <nickkolomietsdooor@gmail.com>
2023-05-20 09:50:58 +0300

    roy algorithm complete writing

--- code/sliders/optimizer_roy/roy.c
+++ code/sliders/optimizer_roy/roy.c
@@ -2,28 +2,50 @@
 #include <stdlib.h>
 
 
-float* roy(float (*func)(float*), float* A, float* B, float eps, int n, int N, int iteration, float w, float a1, float a2){
-    float** X;
-    float** V;
+float** malloc2d(int dim1, int dim2){
+    float** array = (float**)malloc(dim1*sizeof(float*));
+    for (int i = 0; i < dim1; i++){
+        array[i] = (float*) malloc(dim2 * sizeof(float));
+    }
+    return array;
+}
 
+void free2d(float** array, int dim1){
+    for (int i = 0; i< dim1; i++){free(array[i]);}
+    free(array);
+}
+
+
+float rnd(float a, float b){return (float)(rand()) / RAND_MAX * (b - a) + a;}
+
+
+int min_index(float* a, int size){
+    int j = 0;float res = a[0];
+    for (int i = 1; i < size; i++){if (res > a[i]){j = i; res = a[j];}}
+    return j;
+}
+
+
+
+float* roy(float (*func)(float*), float* A, float* B, float eps, int n, int N, int iteration, float w, float a1, float a2){
+    float** X = malloc2d(N, n);
+    float** V = malloc2d(N, n);
+    float** P = malloc2d(N, n);
+    float* res = malloc(N * sizeof(float));
 
 
     for (int i = 0; i < N; i++){
         for (int j = 0; j < n; j++){
             X[i][j] = rnd(A[j], B[j]);
+            P[i][j] = X[i][j];
             V[i][j] = rnd(-eps, eps);
         }
     }
-
-    vector<vector<float>> P;
-    P = X;
-    vector<float> res(N);
-
     for (int i = 0; i < N; i++){res[i] = func(P[i]);}
 
-    pair<int, float> m = min(res);
-    vector<float> b = P[m.first];
-    vector<float> new_res(N);
+    int m = min_index(res, N);
+    float* b = P[m];
+    float* new_res = malloc(N * sizeof(float));
 
     for (int i = 0; i < N; i++){
         for (int j = 0; j < n; j++){
@@ -36,9 +58,8 @@ float* roy(float (*func)(float*), float* A, float* B, float eps, int n, int N, i
             new_res[i] = func(X[i]);
             if (new_res[i] < res[i]){res[i] = new_res[i]; P[i] = X[i];}
         }
-        m = min(res);
-        if (k % 300 == 0){cout<< m.second << endl;}
-        b = P[m.first];
+        m = min(res, N);
+        b = P[m];
         for (int i = 0; i < N; i++){
             for (int j = 0; j < n; j++){
                 V[i][j] = w * V[i][j] + a1 * rnd(0, 1) * (P[i][j] - X[i][j]) + a2 * rnd(0, 1)*(b[j] - X[i][j]);
